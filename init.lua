d876a26dd9aac3cbcf0f4092595cb405b1b0dcce
Allen <allen03archer@gmail.com>
2025-09-24 10:28:49 -0500

    Feat: Added dashboard,telescope to plugins.lua file + create a  keymaps.lua file with leader key mappings for easy navigstion

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- WHAT: Load my core editor settings.
+-- WHY : Keep init.lua clean; settings live in a separate file.
+-- HOW : Require the Lua module we’ll create below.
+ require("allen.options")
+ require("allen.keymaps")
+ require("allen.plugins")
